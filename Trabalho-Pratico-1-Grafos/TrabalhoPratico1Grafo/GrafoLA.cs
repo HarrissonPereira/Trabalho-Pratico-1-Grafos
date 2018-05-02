d5c46b949765f4d00dbf953c776a8748b3a41448
Harrisson Cruz <harrisson.cruz@drlavatudo.com>
2018-05-01 22:00:13 -0300

    Inicio do Exercicio 2

---
+++ Trabalho-Pratico-1-Grafos/TrabalhoPratico1Grafo/GrafoLA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoPratico1Grafo
+{
+    class GrafoLA
+    {
+
+        //Exercício 2
+        private List<Vertice> LA;
+
+
+
+    }
+}
